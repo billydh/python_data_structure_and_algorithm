8f008f230e8a64df3c23bc0f3d3fda33559ec3f9
BillyD <dharmawan.billy@gmail.com>
2019-02-09 18:45:43 +1100

    add max depth of binary tree

---
+++ LeetCode/easy_problems/leetcode_maximum-depth-of-binary-tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxDepth(self, root: 'TreeNode') -> 'int':
+        if root is None:
+            return 0
+
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+
+        return 1 + max(left, right)
+
+
+
+# test cases
+# solution = Solution()
+#
+# [3, 9, 20, null, null, 15, 7]
+# root = TreeNode(3)
+# root.left = TreeNode(9)
+# root.right = TreeNode(20)
+# root.right.left = TreeNode(15)
+# root.right.right = TreeNode(7)
+#
+# solution.maxDepth(root)
